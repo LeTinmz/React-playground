66466677e45955900773a661e4a08ab19d3ce8f0
Tinmar <martin.lecerf@hotmail.fr>
2025-09-02 18:29:05 +0200

    dynamic color type

--- src/Components/ResultList.jsx
+++ src/Components/ResultList.jsx
@@ -1,13 +1,18 @@
 // Components/ResultList.js
 import React from 'react';
-
+import pokeTypeColor from '../utils/pokeTypeColor';
 export const ResultList = ({ pokeList }) => {
     return (
         <ul className="results-list">
-            {pokeList.map((pokemon, index) => (
-                <li key={index} className="pokemon-card">
+            {pokeList.map((pokemon) => (
+                <li key={pokemon.name} className="pokemon-card">
                     <h3 className="pokemon-name">{pokemon.name}</h3>
-                    <span className="pokemon-type">{pokemon.type}</span>
+                    <span
+                        className="pokemon-type"
+                        style={{ background: pokeTypeColor[pokemon.type.toLowerCase()] }}
+                    >
+                        {pokemon.type}
+                    </span>
                 </li>
             ))}
         </ul>
